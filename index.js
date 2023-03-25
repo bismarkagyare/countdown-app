9ccb838e805ade251dfac50b2e71d6d8beaca79c
Bismark Agyare <bismarkagyare54@gmail.com>
2023-03-25 12:58:29 +0000

    added math logic

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ const christmas = '25 Dec 2023';
 const countdown = () => {
   const christmasDate = new Date(christmas);
   const currentDate = new Date();
+
+  const totalSeconds = (christmasDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  console.log(days, hours, minutes, seconds); 
 }
+
+countdown();
+
+//setInterval(countdown, 1000);
